a8c3db7cebb5fe59db8db6c1a4294357feea4c62
nahuelperafan <nahuelperafan@gmail.com>
2022-06-17 14:37:33 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  purge: ["index.html", "./src/**/*.jsx"],
+  darkMode: false, //or 'media or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
